915e12a62ad12ba7547142852cc07f498ab908d1
mgl <matthias.gille-levenson@ens-lyon.fr>
2019-06-20 18:47:52 +0200

    [Collation/] màj

--- Collation/Sortie_17_juin/json_to_xml.sh
+++ Collation/Sortie_17_juin/json_to_xml.sh
@@ -7,10 +7,12 @@ echo "Transformation des fichiers json en fichiers xml"
 # Appel du script transformation_xml_json.py
 for i in chapitres/chapitre*/apparat_collatex.json; do
 chemin=$(dirname "${i}")
+nom_fichier=$(basename -- "$i")
 echo "transformation de $chemin/$i"
+echo "cd $chemin"
 cd $chemin
-echo "python3 ../../transformation_xml_json.py $i"
-python3 ../../transformation_xml_json.py $i
+echo "python3 ../../transformation_xml_json.py $nom_fichier"
+python3 ../../transformation_xml_json.py $nom_fichier
 cd ../..
 echo "Transformation réussie"; done
 # Appel du script transformation_xml_json.py
