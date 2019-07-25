cc16166825f823e12feddba322e0d7803289b59d
Matthias Gille Levenson <matthias.gille-levenson@ens-lyon.fr>
2019-07-25 14:09:46 +0200

    [Collation] création xsl pour tableau alignement + création des tableaux

--- Collation/bash/xml_to_json.sh
+++ Collation/bash/xml_to_json.sh
@@ -19,22 +19,22 @@ if [ $reponse = "o" ]
 then
 # Scission du corpus en dossiers de chapitres
 echo "0 - Scission du corpus, création de dossiers et de fichiers par chapitre"
-java -jar ../$oxygen -o:../tmp/tmp.tmp ../temoins/groupe.xml ../xsl/pre_collation/scission_chapitres.xsl
- Scission du corpus en dossiers de chapitres
+java -jar ../$oxygen -o:../tmp/tmp.tmp ../temoins/groupe.xml ../xsl/pre_alignement/scission_chapitres.xsl
+
 
 # Création des fichiers d'apparat
 echo "I - collation automatique"
 cd ..
 for i in chapitres/chapitre*/juxtaposition.xml; do
 chemin=$(dirname "${i}")
-echo "\n\nI.1 transformation en json"
-java -jar $oxygen -o:$chemin/juxtaposition.json $i xsl/pre_collation/transformation_json.xsl
+echo "\nI.1 transformation en json"
+java -jar $oxygen -o:$chemin/juxtaposition.json $i xsl/pre_alignement/transformation_json.xsl
 
-echo "\n\nI.2 collation du chapitre $chemin"
+echo "\nI.2 collation du chapitre $chemin"
 cd $chemin
 echo "python3 ../../python/collation_python.py juxtaposition.json"
 python3 ../../python/collation_python.py juxtaposition.json;
-echo "Nettoyage du dossier\n\n\n"
+echo "Nettoyage du dossier\n\n"
 rm juxtaposition.json
 rm alignement_collatex.json
 cd ../../; 
