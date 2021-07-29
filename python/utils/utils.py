ba310fe72cc0aaffee29a2fa02e3e5ae832a8eab
Matthias Gille Levenson <matthias.gille-levenson@ens-lyon.fr>
2021-07-29 20:22:57 +0200

    Ajout de fonctions

--- python/utils/utils.py
+++ python/utils/utils.py
@@ -1,3 +1,7 @@
+import glob
+import re
+import subprocess
+
 
 def nettoyage_liste_positions(liste_de_tuples):
     """
@@ -24,3 +28,42 @@ def nettoyage_liste_positions(liste_de_tuples):
     result.sort(key=lambda tup: tup[0])
     return result
 
+
+def run_subprocess(liste):
+    subprocess.run(liste)
+
+
+def txt_to_liste(filename):
+    """
+    Transforme le fichier txt produit par Freeling ou pie en liste de listes pour processage ultérieur.
+    :param filename: le nom du fichier txt à transformer
+    :return: une liste de listes: pour chaque forme, les différentes analyses
+    """
+    output_list = []
+    fichier = open(filename, 'r')
+    for ligne in fichier.readlines():
+        if not re.match(r'^\s*$',
+                        ligne):  # https://stackoverflow.com/a/3711884 élimination des lignes vides (séparateur de phrase)
+            ligne_splittee = re.split(r'\s+', ligne)
+            output_list.append(ligne_splittee)
+    return output_list
+
+
+def sigles():
+    """
+    Cette fonction retourne une liste de sigles à partir de tous les témoins
+    """
+    return [fichier.split("/")[1].split(".xml")[0] for fichier in glob.glob('temoins_tokenises/*.xml')]
+
+
+def chemin_fichiers_finaux(div: str):
+    """
+    Cette fonction retourne la liste des fichiers XML finaux d'une division donnée
+    """
+    return [fichier for fichier in glob.glob(f'divs/div{div}/apparat_*_{div}_final.xml')]
+
+def chemin_temoins_tokenises():
+    """
+    Cette fonction retourne la liste des chemins vers les fichiers tokénisés
+    """
+    return [fichier for fichier in glob.glob('temoins_tokenises/*.xml')]
