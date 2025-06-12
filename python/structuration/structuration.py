effa76b2f8c4678e5e17cb74cc197919b98c42d4
Matthias Gille Levenson <matthias.gille-levenson@ens-lyon.fr>
2025-06-12 15:08:14 +0200

    Rm debug file

--- python/structuration/structuration.py
+++ python/structuration/structuration.py
@@ -76,8 +76,8 @@ def check_if_match(json_table: str, target_id: str) -> (bool, str):
     aligned_table = list(zip([token[0] if token else None for token in json_table['table'][0]],
                              [token[0] if token else None for token in json_table['table'][1]]))
     test_file_writing(object=aligned_table, name="aligned.json", format="json")
-    with open("/home/mgl/Documents/test/json_aligned_table.json", "w") as output_table:
-        json.dump(aligned_table, output_table)
+    # with open("/home/mgl/Documents/test/json_aligned_table.json", "w") as output_table:
+        # json.dump(aligned_table, output_table)
 
     # Ici on ne va chercher que le pivot, ce qui n'est pas suffisant parfois (cas de la ponctuation). Il
     # Faudrait trouver une méthode avec plus de contexte.
